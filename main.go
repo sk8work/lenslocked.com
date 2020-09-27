e2ac4895f51ae3280c41e1369cb9a208a40d29f3
sk8work <eshikhantsev@gmail.com>
2020-09-27 08:31:18 +0300

    Added a contact page

--- main.go
+++ main.go
@@ -13,8 +13,15 @@ func main() {
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Println("Someone visited our page")
-	fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
+	// fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
+	// fmt.Println("Someone visited our page")
+	// fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
+	// fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
-// video 6
+// video 7
