07c0c977d9d6475c2821b8db658e1a319995c067
sk8work <eshikhantsev@gmail.com>
2020-09-27 18:52:17 +0300

    Added a 404 page

--- main.go
+++ main.go
@@ -18,10 +18,13 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if uoy keep being sent to invalid page <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a></p>")
 	}
 	// fmt.Println("Someone visited our page")
 	// fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
 	// fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
-// video 7
+// video 8
