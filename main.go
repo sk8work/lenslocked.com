d80040bbc68d33931a61c49d91e13486f136cbb8
sk8work <eshikhantsev@gmail.com>
2020-09-30 11:37:37 +0300

    mux

--- main.go
+++ main.go
@@ -6,25 +6,24 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+
+	// http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":8080", mux)
 
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	// fmt.Fprint(w, r.URL.Path)
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
-	} else if r.URL.Path == "/contact" {
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
 		fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if uoy keep being sent to invalid page <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a></p>")
 	}
-	// fmt.Println("Someone visited our page")
-	// fmt.Fprint(w, "<h1>Welcome to my super duper site</h1>")
-	// fmt.Fprint(w, "To get in touch? please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
-// video 8
+// video 9
