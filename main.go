8ed8b6db5d3230a32e96331b0680bb67cd5d734c
sk8work <eshikhantsev@gmail.com>
2020-09-30 11:52:13 +0300

    julienschmidt_httprouter

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handlerFunc)
+
+	router := httprouter.New()
+	router.GET("/hello/:name", Hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 	// http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", mux)
+	// http.ListenAndServe(":8080", nil)
 
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
